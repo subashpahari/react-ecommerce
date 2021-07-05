57421d27f488cdcb954a52275968e1917fca004a
Subash Pahari <subashpahari.12345gmail.com>
2021-07-05 19:58:06 +0545

    initialized user reducer

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import {BrowserRouter} from 'react-router-dom';
+import {Provider} from 'react-redux';
 
 
 ReactDOM.render(
-  <BrowserRouter>
+  <Provider>
+    <BrowserRouter>
     <App />
-  </BrowserRouter>,
+  </BrowserRouter>
+  </Provider>,
   document.getElementById("root")
 );
 
